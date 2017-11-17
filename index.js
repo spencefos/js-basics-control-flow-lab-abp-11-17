164a3726f6610e67c72e7c6482f6ac080c1ed391
Spencer Foster <fosterjs@me.com>
2017-11-17 04:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 function scuberGreetingForFeet(feet){
-  if(feet <=400){
+  if(feet <= 400){
     return 'This one is on me!'
   } else if (1999 < feet && feet < 2500){
-    return 'I will gladly take your thirty bucks.'
+      return "I will gladly take your thirty bucks."
   } else {
-    return 'No can do.'
+    return "No can do."
   }
 }
 
 function ternaryCheckCity(city){
-  if (city === 'NYC'){
-    return 'Ok, sounds good.'
-  }else {
-    return 'No go.'
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
   }
 }
-function switchOnCharmFromTip(tipString){
-  switch(tipString){
-    case "generous":
-      return "Thank you so much."
-      break;
-    case "not as generous":
-      return "Thank you."
-      break;
-    case "thanks for everything"
-      return "Bye."
-  }
