1c5410aeaafda3623b97aa89a91d0d14365c8d45
Spencer Foster <fosterjs@me.com>
2017-11-17 04:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-function scuberGreetingForFeet(distance) {
-  if(distance <= 400)
+function scuberGreetingForFeet(feet){
+  if(feet <=400){
     return 'This one is on me!'
-  } else if(distance > 2000)
-    return 'I will gladly take your thirty bucks.'
+  }
 }
