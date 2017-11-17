ab59e501da5c6fc1174760ee7c7c1d77704bc043
Spencer Foster <fosterjs@me.com>
2017-11-17 04:24:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function scuberGreetingForFeet(feet){
   if(feet <=400){
     return 'This one is on me!'
+  } else if (1999 < feet && feet < 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
   }
 }
